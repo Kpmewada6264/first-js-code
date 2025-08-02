34c30219314275d9f03cc88c04eec74f69c523b5
Krishnapal Mewada <krishnamewada417@gmail.com>
2025-08-02 04:32:30 +0000

    variables

---
+++ variable.js
@@ -0,0 +1,16 @@
+const account_Id = 574756
+let accountName = 'John Doe'
+var accountEmail = 'krishna@123.com'
+accountCity = 'New York'
+
+
+// accountId=2   //not allowed
+
+
+
+accountEmail = 'hc@hc.com'
+accountName = 'Krishna'
+accountCity = 'San Francisco'
+
+console.table([account_Id,accountEmail,accountName,accountCity])
+
