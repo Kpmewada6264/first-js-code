38031b2f84e7bf53ec3e755cfa1d70d15dd82e20
Krishnapal Mewada <krishnamewada417@gmail.com>
2025-08-02 15:13:47 +0000

    operations

---
+++ operation.js
@@ -0,0 +1,59 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)    //-3
+
+console.log(2+2)  //4
+console.log(2-2)  //0
+console.log(2*2)  //4
+console.log(2/2)  //1
+console.log(2%2)  //0
+console.log(2**4) //16 
+
+let str1="hello"
+let str2=" krishna "
+
+let str3=str1+str2
+
+console.log(str3)  //hello krishna
+
+console.log("1"+2);  //12
+console.log(1+"2");  //12
+console.log("1"+"2"); //12
+
+console.log(1+2+"3");  //33
+
+console.log(3+4*5%3);  
+
+console.log(true);
+
+
+console.log(+true); //1
+
+console.log(+""); //0
+
+let num1,num2,num3
+
+num1=num2=num3=2+2   //it is tricky behavior
+
+
+let gamecounter=100
+++gamecounter  //pre increament
+console.log(gamecounter);
+
+gamecounter++  //post increament
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
