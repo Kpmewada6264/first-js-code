6c602965bdcfe65fa0843601f785a20925c01b56
Krishnapal Mewada <krishnamewada417@gmail.com>
2025-08-02 15:33:34 +0000

    comparision

---
+++ comparitions.js
@@ -0,0 +1,31 @@
+console.log(2>1);  //true
+console.log(2>=1); //true 
+console.log(2<1);  //false
+console.log(2<=1); //false
+console.log(2==1);  //false
+console.log(2!=1);  //true
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+
+console.log(null==0);
+
+console.log(null>=0);
+
+
+console.log(undefined>0);
+
+console.log(undefined==0);
+
+console.log(undefined>=0);
+
+//===
+
+console.log("2"==2); //== and === work differently
+
+console.log("2"===2);
+
+
