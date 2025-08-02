0aad55d326aac9b0db92ecabd12c5c7fe97ac3e4
Krishnapal Mewada <krishnamewada417@gmail.com>
2025-08-02 05:21:43 +0000

    datatype conversion

---
+++ datatypeconversions.js
@@ -0,0 +1,67 @@
+let score = 33
+let num = "33avb"
+
+console.log(typeof score)
+console.log(typeof num)
+
+let valueInNumber=Number(num)  //convert in number
+
+console.log(typeof valueInNumber)//number 
+
+console.log(valueInNumber) // NaN => Not a Number
+
+
+
+
+let number = null
+
+
+console.log(typeof number)
+
+let valueInNumbers=Number(number)  //convert in number
+
+console.log(typeof valueInNumbers)//number 
+
+console.log(valueInNumbers) //  0
+
+
+
+let numbers 
+
+
+console.log(typeof numbers)
+
+let valueInNum=Number(number)  //convert in number
+
+console.log(typeof valueInNum)//number 
+
+console.log(valueInNum) // 0
+
+
+
+let isloggedin=true
+
+console.log(typeof isloggedin) //boolean
+
+
+let valueInNums=Number(isloggedin)  //convert in number
+
+console.log(typeof valueInNums)//number 
+
+console.log(valueInNums) // 0
+
+
+
+//"33"=> 33
+//"33ad"=> nan
+//true=>1,false=>0
+
+let istrue=1
+
+let booleanistrue=Boolean(istrue)
+console.log(booleanistrue) //true
+
+
+let somenumber=33
+let numstostr=String(somenumber)
+console.log(numstostr)
